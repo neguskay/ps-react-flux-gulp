109356cf5a483358a143cba93de0bfcbeb8911b4
Sidney <sofosu-ottopah@zaizi.com>
2019-03-18 17:02:37 +0000

    updated comments

--- src/components/authors/authorPage.js
+++ src/components/authors/authorPage.js
@@ -1,22 +1,26 @@
-"use strict";
+'use strict';
 
 var React = require('react');
 var AuthorApi = require('../../api/authorApi');
 var AuthorList = require('./authorList');
 
+//Author Page Component
 var AuthorPage = React.createClass({
+	//set/Get the initial state of teh authors
 	getInitialState: function() {
 		return {
 			authors: []
 		};
 	},
 
+	//Fetch some data if component actually mounted
 	componentDidMount: function() {
 		if (this.isMounted()) {
 			this.setState({ authors: AuthorApi.getAllAuthors() });
 		}
 	},
 
+	//The render method to be returned
 	render: function() {
 		return (
 			<div>
@@ -27,4 +31,5 @@ var AuthorPage = React.createClass({
 	}
 });
 
+//Module export
 module.exports = AuthorPage;
